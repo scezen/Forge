f23e426b3eb54efaeaf04517835fe5b2b23f1c72
scezen <nootybear@gmail.com>
2024-05-22 14:36:23 +0200

    Refacto

--- src/main/java/com/scezen/forge/controller/ImageGenController.java
+++ src/main/java/com/scezen/forge/controller/ImageGenController.java
@@ -1,18 +1,14 @@
 package com.scezen.forge.controller;
 
 import com.scezen.forge.service.ImageGenService;
-import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.image.ImageResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.Map;
 
 @RestController
 public class ImageGenController {
@@ -29,15 +25,9 @@ public class ImageGenController {
     @GetMapping("/getimage")
     public void generateImage(HttpServletResponse response) throws IOException {
 
-        PromptTemplate promptTemplate = new PromptTemplate(imagePromptResource);
+        String promptContent = new PromptTemplate(imagePromptResource).create().getContents();
 
-        Prompt prompt = promptTemplate.create();
-
-        String promptContent = prompt.getContents();
-
-        ImageResponse imageResponse = imageGenService.generateImage(promptContent);
-
-        String imageUrl = imageResponse.getResult().getOutput().getUrl();
+        String imageUrl = imageGenService.generateImage(promptContent).getResult().getOutput().getUrl();
 
         response.sendRedirect(imageUrl);
     }
